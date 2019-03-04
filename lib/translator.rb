e5dc08cd691251c0810b1ce0f8dddd9f284c906f
Fillan Lee <fillanlee@aol.com>
2019-03-04 02:13:21 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,6 @@
 # require modules here
 require "pry"
 require "yaml"
-# emoticons = YAML.load_file('lib/emoticons.yml')
-# # emoticons
 
 def load_library (path)
   new_hash = {}
@@ -10,21 +8,14 @@ def load_library (path)
   
    new_hash["get_emoticon"] = {}
    new_hash["get_meaning"] = {}
-  
 
 english = 0
 japan = 1
 
   emoticons.each do |word, emot_array|
-    # emot_array.each do |emoji_type|
-      
+ 
   new_hash["get_meaning"][emot_array[japan]] = word
   new_hash["get_emoticon"][emot_array[english]] = emot_array[japan]
-   
-    #   new_hash["get_meaning"][emot_array[english]] = emot_array[japan]
-    #   new_hash["get_emoticon"][emot_array[japan]] = emot_desc emot_array[english]
-    # 
-    # end
   end
   new_hash
 end
@@ -33,17 +24,14 @@ end
 
 def get_japanese_emoticon(path, emoji)
   yaml_hash = load_library(path)
-  
   converted = yaml_hash["get_emoticon"][emoji]
   
   if converted
     converted 
-    # = yaml_hash["get_emoticon"][emoji]
   else
-   converted = "Sorry, that emoticon was not found"
+    converted = "Sorry, that emoticon was not found"
   end
   converted
-  
 end
 
 def get_english_meaning(path, emoji)
@@ -54,7 +42,7 @@ def get_english_meaning(path, emoji)
   if results
     results
   else
-   results = "Sorry, that emoticon was not found"
+    results = "Sorry, that emoticon was not found"
   end
   results
 end
